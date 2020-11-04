b81f06615232d8cc215631906dd605a400927642
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-11-04 23:14:32 +0530

    [Kanishk]Added:UC17:to update contact details

--- main/java/com/addressbookjdbc/AddressBookService.java
+++ main/java/com/addressbookjdbc/AddressBookService.java
@@ -5,12 +5,34 @@ import java.util.List;
 public class AddressBookService
 {
 	public AddressBookJDBCService addressbookJDBCService;
-	
+	private List<AddressBookData> addressBookDataList;
 	public AddressBookService(){
-		this.addressbookJDBCService = new AddressBookJDBCService();
+		this.addressbookJDBCService = AddressBookJDBCService.getInstance();
 	}
 	
 	public List<AddressBookData> readAddressBookData() throws AddressBookJDBCException{
 		return this.addressbookJDBCService.readData();
+	}
+	public void updateContactDetails(String name,String state,int zip) throws AddressBookJDBCException
+	{
+		int result=new AddressBookJDBCService().updateAddressBookDataUsingPreparedStatement(state,zip,name);
+		if(result==0)
+			return;
+		AddressBookData addressBookData=this.getAddressBookData(name);
+		if(addressBookData!=null) 
+			{
+			addressBookData.setZip(zip);
+			addressBookData.setState(state);
+			}
+	}
+	private AddressBookData getAddressBookData(String name) {
+		return this.addressBookDataList.stream()
+				.filter(addressBookDataListObject->addressBookDataListObject.getFirst_name().equals(name))
+				.findFirst().orElse(null);
+	}
+
+	public boolean checkAddressBookInSyncWithDB(String name) throws AddressBookJDBCException {
+		List<AddressBookData> addressBookDataList=new AddressBookJDBCService().getAddressBookDataFromDB(name);
+		return addressBookDataList.get(0).equals(getAddressBookData(name));
 	}
 }
--- test/java/com/addressbookjdbc/AddressBookJDBCTest.java
+++ test/java/com/addressbookjdbc/AddressBookJDBCTest.java
@@ -11,12 +11,23 @@ public class AddressBookJDBCTest
 {
 	//UC16
     @Test
-    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws AddressBookJDBCException
+    public void givenAddressBookDataInDB_WhenRetrieved_ShouldMatchCount() throws AddressBookJDBCException
     {
     	List<AddressBookData> addressbookdata;
     	AddressBookService addressBookService = new AddressBookService();
 		addressbookdata =addressBookService.readAddressBookData();
 		Assert.assertEquals(4, addressbookdata.size());
+    }
+    //UC17
+    @Test
+    public void givenAddressBookInDB_WhenUpdatedUsingPreparedStatement_ShouldSyncWithDB() throws AddressBookJDBCException
+    {
+    	List<AddressBookData> addressbookdata;
+    	AddressBookService addressBookService = new AddressBookService();
+		addressbookdata =addressBookService.readAddressBookData();
+		addressBookService.updateContactDetails("Kanishk","Karnataka",22100);
+		boolean result=addressBookService.checkAddressBookInSyncWithDB("Kanishk");
+		Assert.assertTrue(result);
     }
 }
 
